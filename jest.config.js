e26c3d826eec17b407f689348cbb1e6a59e131dd
tjamesmac <tjamesmac@gmail.com>
2019-07-15 15:18:48 -0700

    Add enzyme tools

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,10 @@ module.exports = {
     "<rootDir>/src"
   ],
   "transform": {
-    "^.+\\.tsx?$": "ts-jest"
-  }
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  // Enzyme setup
+  "snapshotSerializers": ["enzyme-to-json/serializer"],
+  // setupTestFrameworkScriptFile is decprecated
+  "setupFilesAfterEnv": ["<rootDir>/src/setupEnzyme.ts"],
 }
