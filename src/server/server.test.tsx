de9aefe9957b287f5e9cb7dfa5a077cf920f3904
tjamesmac <tjamesmac@gmail.com>
2019-07-18 16:08:40 -0700

    Fix formatting

--- src/server/server.test.tsx
+++ src/server/server.test.tsx
@@ -1,15 +1,13 @@
 import request from 'supertest';
 import Server from './server';
 
-it('runs instance of server class', () => {
-  const server: any = new Server(4000);
-  expect(server).toBeInstanceOf(Server);
-});
-
-describe('Test the root path', () => {
+describe('my server', () => {
   const server = new Server(4000);
   const app = server.getApp();
-  test('it should response the GET method', (done) => {
+  it('runs instance of server class', () => {
+    expect(server).toBeInstanceOf(Server);
+  });
+  it('should respond with 200', (done) => {
     request(app).get('/').then((response: any) => {
       expect(response.statusCode).toBe(200);
       done();
