df4d57341d5ee1d003145f1e3f0f270d7b8266d7
tjamesmac <tjamesmac@gmail.com>
2019-07-18 15:43:37 -0700

    Starting on server class

--- src/server/server.tsx
+++ src/server/server.tsx
@@ -1,26 +1,29 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-const PORT: number = 3000;
+import express from 'express';
+import * as path from 'path';
 
-const graphqlHTTP = require('express-graphql');
-const { buildSchema } = require('graphql');
-
-const schema = buildSchema(`
-  type Query {
-    hello: String
+class Server {
+  private MSG: string = 'THIS IS MY SERVER RUNNING';
+  private app: express.Application = express();
+  private PORT: number;
+  constructor(portNumber: number) {
+    this.PORT = portNumber;
   }
-`);
+  public getApp() {
+    this.getIndex();
+    this.getDist();
+    return this.app;
+  }
+  public serverMSG() {
+    return `${this.MSG} ${this.PORT}`;
+  }
+  public getDist() {
+    this.app.use('/', express.static(path.join(__dirname, '../../dist')));
+  }
+  public getIndex() {
+    this.app.get('/', (req: express.Request, res: express.Response) => {
+      res.sendFile(path.join(__dirname, '../../dist/index.html'));
+    });
+  }
+}
 
-const root = { hello: () => 'Hello world!' };
-
-app.use('/', express.static(path.join(__dirname, '../../dist')));
-app.use('/graphql', graphqlHTTP({
-  schema: schema,
-  rootValue: root,
-  graphiql: true,
-}));
-app.get('/', (req: Request, res: any) => {
-  res.sendFile(path.join(__dirname, '../../dist/index.html'));
-});
-app.listen(PORT, () => console.log(`example app listening on ${PORT}`));
+export default Server;
