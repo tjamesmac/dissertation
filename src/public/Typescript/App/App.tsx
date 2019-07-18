6a13c92c1ca5d5940246523930f1002f56c8f656
tjamesmac <tjamesmac@gmail.com>
2019-07-18 15:42:31 -0700

    Add App file

---
+++ src/public/Typescript/App/App.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { Hello } from '../Hello/Hello';
+
+export const App = () => {
+  return(
+    <Hello name={'Thomas'}/>
+  );
+};
--- src/public/Typescript/index.test.tsx
+++ src/public/Typescript/index.test.tsx
@@ -1,8 +1,11 @@
 import { shallow } from 'enzyme';
 import * as React from 'react';
-import { Hello } from './Hello/Hello';
+import { App } from './index';
 
 it('renders the heading with a name', () => {
-  const result = shallow(<Hello name='Test'/>).contains(<h1>Hello, Test. Welcome back!</h1>);
+  const result = shallow(<App/>).contains(
+  <div>
+    <h1 className='header'>Hello, Thomas. Welcome back!</h1>
+  </div>);
   expect(result).toBeTruthy();
 });
