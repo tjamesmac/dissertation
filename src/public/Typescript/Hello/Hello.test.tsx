0863a05ae256b860a06d366148c6d6a6f905fbe9
tjamesmac <tjamesmac@gmail.com>
2019-07-18 16:08:16 -0700

    Fix formatting

--- src/public/Typescript/Hello/Hello.test.tsx
+++ src/public/Typescript/Hello/Hello.test.tsx
@@ -3,12 +3,19 @@ import * as React from 'react';
 import renderer from 'react-test-renderer';
 import { Hello } from './Hello';
 
-describe('Test Hello component', () => {
-  it('renders the heading with a name', () => {
-    const result = shallow(<Hello name='Test'/>).contains(
+describe('Hello component', () => {
+  const name = 'Test';
+  it('renders the Hello with a name', () => {
+    const result = shallow(<Hello name={name}/>).contains(
     <div>
       <h1 className='header'>Hello, Test. Welcome back!</h1>
     </div>);
     expect(result).toBeTruthy();
+  });
+  it('it renders correctly', () => {
+    const tree = renderer
+      .create(<Hello name={name}/>)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
