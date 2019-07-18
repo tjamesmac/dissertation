e238de1df8ee7952e085324277eaaabc2439667b
tjamesmac <tjamesmac@gmail.com>
2019-07-18 15:43:20 -0700

    Change app import

--- src/public/Typescript/index.test.tsx
+++ src/public/Typescript/index.test.tsx
@@ -1,6 +1,6 @@
 import { shallow } from 'enzyme';
 import * as React from 'react';
-import { App } from './index';
+import { App } from './App/App';
 
 it('renders the heading with a name', () => {
   const result = shallow(<App/>).contains(
