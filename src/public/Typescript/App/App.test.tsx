4c46d51287a72147d889800ebd02ac8bbab76521
tjamesmac <tjamesmac@gmail.com>
2019-07-18 16:07:58 -0700

    Fix formatting

--- src/public/Typescript/App/App.test.tsx
+++ src/public/Typescript/App/App.test.tsx
@@ -1,11 +1,17 @@
 import { shallow } from 'enzyme';
 import * as React from 'react';
+import renderer from 'react-test-renderer';
 import { App } from '../App/App';
 
-it('renders the heading with a name', () => {
-  const result = shallow(<App/>).contains(
-  <div>
-    <h1 className='header'>Hello, Thomas. Welcome back!</h1>
-  </div>);
-  expect(result).toBeTruthy();
+describe('App component renders correctly', () => {
+  it('renders as expected', () => {
+    const result = shallow(<App/>);
+    expect(result.exists()).toBe(true);
+  });
+  it('matches snapshot', () => {
+    const tree = renderer
+      .create(<App />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
