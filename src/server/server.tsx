2506af9ca05ad2516ebd94ea5813ebc7ceed0a05
tjamesmac <tjamesmac@gmail.com>
2019-07-19 13:11:06 -0700

    Refactor server

--- src/server/server.tsx
+++ src/server/server.tsx
@@ -1,29 +1,26 @@
+import bodyParser from 'body-parser';
 import express from 'express';
 import * as path from 'path';
 
 class Server {
   private MSG: string = 'THIS IS MY SERVER RUNNING';
-  private app: express.Application = express();
-  private PORT: number;
-  constructor(portNumber: number) {
-    this.PORT = portNumber;
+  public app: express.Application;
+  constructor() {
+    this.app = express();
+    this.config();
   }
-  public getApp() {
-    this.getIndex();
-    this.getDist();
-    return this.app;
-  }
-  public serverMSG() {
-    return `${this.MSG} ${this.PORT}`;
-  }
-  public getDist() {
+  // public getApp() {
+  //   this.getIndex();
+  //   this.getDist();
+  //   return this.app;
+  // }
+  private config(): void {
+    this.app.use(bodyParser.json());
     this.app.use('/', express.static(path.join(__dirname, '../../dist')));
-  }
-  public getIndex() {
     this.app.get('/', (req: express.Request, res: express.Response) => {
       res.sendFile(path.join(__dirname, '../../dist/index.html'));
     });
   }
 }
 
-export default Server;
+export default new Server().app;
