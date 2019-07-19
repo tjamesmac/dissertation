a0567a7bcb909aca257548f5a95b67a5f671b1d7
tjamesmac <tjamesmac@gmail.com>
2019-07-19 13:20:19 -0700

    remove unneeded test

--- src/server/server.test.tsx
+++ src/server/server.test.tsx
@@ -2,11 +2,7 @@ import request from 'supertest';
 import Server from './server';
 
 describe('my server', () => {
-  const server = new Server(4000);
-  const app = server.getApp();
-  it('runs instance of server class', () => {
-    expect(server).toBeInstanceOf(Server);
-  });
+  const app = new Server().app;
   it('should respond with 200', (done) => {
     request(app).get('/').then((response: any) => {
       expect(response.statusCode).toBe(200);
