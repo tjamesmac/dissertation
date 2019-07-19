a71c2d9f52457f9ededb384ae18215f76c54b30a
tjamesmac <tjamesmac@gmail.com>
2019-07-19 14:10:34 -0700

    Add extra tests for htmlTemplate

--- src/server/server.test.tsx
+++ src/server/server.test.tsx
@@ -1,5 +1,10 @@
+import { mount, shallow  } from 'enzyme';
 import request from 'supertest';
 import Server from './server';
+
+import React from 'react';
+import { Hello } from '../public/Typescript/Hello/Hello';
+import { htmlTemplate } from './htmlTemplate';
 
 describe('my server', () => {
   const app = new Server().app;
@@ -8,5 +13,25 @@ describe('my server', () => {
       expect(response.statusCode).toBe(200);
       done();
     });
+  });
+
+});
+describe('server template', () => {
+  it('should match template with no props', () => {
+    const html = htmlTemplate();
+    expect(html).toMatch(htmlTemplate());
+  });
+  it('prop should render with correct value', () => {
+    const props = {
+      name: 'Test',
+    };
+    const helloComponent = mount(<Hello {...props} />);
+    expect(helloComponent.prop('name')).toEqual('Test');
+  });
+  it('should match template', () => {
+    const name = 'Test';
+    const hello = <Hello name={name}/>;
+    const html = htmlTemplate(hello);
+    expect(html).toMatch(htmlTemplate(hello));
   });
 });
