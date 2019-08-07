23134649af84bac8162a2a637daa81ecfcf40323
tjamesmac <tjamesmac@gmail.com>
2019-08-07 13:45:04 -0700

    Fix div value not working

--- src/public/Typescript/Main/helpers.tsx
+++ src/public/Typescript/Main/helpers.tsx
@@ -1,7 +1,11 @@
+import { IWord } from '../../../server/helpers/wordProcessing';
+
 export const submission = async (event: React.FormEvent) => {
   event.preventDefault();
 
-  const textAreaValue: string = (document.getElementById('textarea') as HTMLTextAreaElement).value;
+  const textAreaValue: string =
+  (document.getElementById('textarea') as HTMLTextAreaElement)
+  .innerHTML;
 
   const bodyText: object = {value: textAreaValue};
 
@@ -19,8 +23,20 @@ export const submission = async (event: React.FormEvent) => {
     if (response.status === 200) {
       const responseJson = await response.json();
       console.log(responseJson, 'response');
+      validateWords(responseJson[0], textAreaValue);
     }
   } catch (error) {
     console.error('uh oh error', error);
+  }
+};
+
+export const validateWords = (response: IWord, checkString: string) => {
+  console.log(checkString);
+  console.log(response.word);
+  if (checkString.includes(response.word)) {
+    console.log('i found the target');
+
+  } else {
+    console.log('oh bother');
   }
 };
