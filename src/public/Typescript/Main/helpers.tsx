93137c546352d633375df73e2aa56ca81ad875fd
tjamesmac <tjamesmac@gmail.com>
2019-08-12 15:28:54 -0700

    Add synonym start

--- src/public/Typescript/Main/helpers.tsx
+++ src/public/Typescript/Main/helpers.tsx
@@ -24,8 +24,12 @@ export const submission = async (event: React.FormEvent) => {
     const response = await data;
     if (response.status === 200) {
       const responseJson = await response.json();
+      console.log(response.status);
       console.log(responseJson, 'response');
-      validateWords(responseJson, textAreaValue);
+      const textChange = validateWords(responseJson, textAreaValue);
+      (document.getElementById('textarea') as HTMLDivElement)
+      .innerHTML = textChange;
+      showSynonyms(responseJson);
     }
   } catch (error) {
     console.error('uh oh error', error);
@@ -42,14 +46,46 @@ export const validateWords = (response: any, checkString: string) => {
       const regex = new RegExp(find, 'g');
       // string doesn't replace unless assigned to variable
       const newString = toChange.replace(regex, `<span style='color: green'>${key.word}</span>`);
-      // assign changes to the og string
+      // assign changes to the original string
       toChange = newString;
       // now I need to create a dropdown menu and populate the synonyms
     } else {
       console.log('oh bother');
     }
   }
-  (document.getElementById('textarea') as HTMLDivElement)
-  .innerHTML = toChange;
+  return toChange;
+};
 
+export const showSynonyms = (response: any) => {
+  const text = (document.querySelector('#textarea') as HTMLDivElement);
+
+  if (text) {
+    const children: any = text.children;
+    for (let item of children) {
+      console.log(item);
+      console.log(item.innerHTML);
+      for (let key of response) {
+        if (key.word === item.innerHTML) {
+          console.log('oh my god I got a match');
+          console.log(key.synonyms);
+        }
+      }
+      // const div = document.createElement('div');
+      // item.addEventListener('mouseenter', ( event: any ) => {
+      //   // conditionally render the component
+      //   // when I get the match
+      //   console.log('hello i am in the text area');
+      //   console.log(item);
+      //   const body = document.getElementsByTagName('body')[0];
+      //   div.classList.add('synonym');
+      //   body.append(div);
+
+      // });
+      // item.addEventListener('mouseleave', ( event: any ) => {
+      //   console.log('hello i am going now');
+      //   div.classList.remove('synonym');
+      // });
+    }
+  }
+  console.log(text);
 };
