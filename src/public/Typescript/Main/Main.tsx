5b6c8a21725ad76af9a3bbbeef072ce264e283f7
tjamesmac <tjamesmac@gmail.com>
2019-08-12 15:29:15 -0700

    Add modal condtional render

--- src/public/Typescript/Main/Main.tsx
+++ src/public/Typescript/Main/Main.tsx
@@ -6,17 +6,26 @@ import { submission } from './helpers';
 export const Main: React.FunctionComponent = () => {
   const [words, setWords] = React.useState('');
   const [updatedWord, setUpdatedWords] = React.useState('');
+  const [modalState, setModalState] = React.useState(false);
 
   const getWords = () => {
     const textArea = (document.getElementById('textarea') as HTMLTextAreaElement);
     if (textArea !== null) {
       const value = textArea.innerHTML;
+      console.log(value);
       setWords(value);
       console.log(words);
     } else {
       console.log('i am not working');
     }
   };
+
+  const Modal = () => <div>This is my modal</div>;
+  let showModal;
+  if (modalState) {
+    showModal = <Modal />
+  }
+  const show = () => setModalState(true);
 
   return (
     <div className='container'>
@@ -25,6 +34,8 @@ export const Main: React.FunctionComponent = () => {
           <form onSubmit={(e: React.FormEvent) => submission(e)}>
             <div className='row'>
               <div className='col-12'>
+              {showModal};
+              <button onClick={() => show()}>hello button</button>
               <label>Please enter a gender</label>
               <input></input>
               </div>
