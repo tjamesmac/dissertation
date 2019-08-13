bd7d5aeffdea2a2ead4ef2c0179cdccf7324bdfb
tjamesmac <tjamesmac@gmail.com>
2019-08-13 16:27:45 -0700

    Fix modal displaying synonyms

--- src/public/Typescript/Main/Main.tsx
+++ src/public/Typescript/Main/Main.tsx
@@ -1,41 +1,98 @@
 import * as React from 'react';
-import { submission } from './helpers';
+import { IResponse, validateWords  } from './helpers';
+
 
 // export interface IMainProps { name: string; }
-
+interface IModalProps { words: string[]; }
+const Modal = (prop: IModalProps) => {
+  const words: string[] = prop.words;
+  const wordMap = words.map((word: string, index: number) => {
+    return <li key={index}>{word}</li>;
+  });
+  return (
+    <ul className='synonym'>
+      {wordMap}
+    </ul>
+  );
+};
 export const Main: React.FunctionComponent = () => {
-  const [words, setWords] = React.useState('');
-  const [updatedWord, setUpdatedWords] = React.useState('');
-  const [modalState, setModalState] = React.useState(false);
+  const [words, setWords] = React.useState< null | IResponse[] >(null);
+  const [modalState, setModalState] = React.useState< false | true >(false);
 
-  const getWords = () => {
-    const textArea = (document.getElementById('textarea') as HTMLTextAreaElement);
-    if (textArea !== null) {
-      const value = textArea.innerHTML;
-      console.log(value);
-      setWords(value);
-      console.log(words);
-    } else {
-      console.log('i am not working');
+  const submission = async (event: React.FormEvent) => {
+    event.preventDefault();
+    const textAreaValue: string =
+    (document.getElementById('textarea') as HTMLDivElement)
+    .innerHTML;
+    const bodyText: object = {value: textAreaValue};
+    try {
+      const URL = 'http://localhost:3000/';
+      const data = await fetch(URL, {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(bodyText),
+      });
+      const response = await data;
+      if (response.status === 200) {
+        const responseJSON: IResponse[] = await response.json();
+        const textChange = validateWords(responseJSON, textAreaValue);
+        (document.getElementById('textarea') as HTMLDivElement)
+        .innerHTML = textChange;
+
+        setWords(responseJSON);
+
+      }
+    } catch (error) {
+      console.error('uh oh error', error);
     }
   };
 
-  const Modal = () => <div>This is my modal</div>;
   let showModal;
-  if (modalState) {
-    showModal = <Modal />
+  if (words) {
+    for (let word of words) {
+      console.log('inside state loop');
+      console.log(word);
+      console.log(word.word);
+      // const textAreaValue: string =
+      //   (document.getElementById('textarea') as HTMLDivElement)
+      //   .innerHTML;
+      const textAreaValue: Element | null = document.querySelector('#textarea');
+      console.log(textAreaValue);
+      if (textAreaValue) {
+        // type of HTMLCollection not recognised as array
+        const children: any = textAreaValue.children;
+        console.log(children);
+        for (let element of children) {
+          console.log(element);
+          if (word.word === element.innerHTML) {
+            console.log('oh boy i got it');
+            if (modalState) {
+              showModal = <Modal words={word.synonyms}/>;
+            }
+          } else {
+            console.log('I dont equal');
+          }
+        }
+      }
+    }
   }
-  const show = () => setModalState(true);
 
+  const show = () => setModalState(!modalState);
+  console.log('please');
+  console.log(words);
   return (
     <div className='container'>
       <div className='row'>
         <div className='col-12'>
+          <button type='button' onClick={() => show()}>hello button</button>
+          {showModal}
           <form onSubmit={(e: React.FormEvent) => submission(e)}>
             <div className='row'>
               <div className='col-12'>
-              {showModal};
-              <button onClick={() => show()}>hello button</button>
+
               <label>Please enter a gender</label>
               <input></input>
               </div>
@@ -43,7 +100,7 @@ export const Main: React.FunctionComponent = () => {
             <div className='row'>
               <div className='col-12'>
               <label>Please enter an advert</label>
-              <div id='textarea' onChange={() => getWords()} contentEditable></div>
+              <div id='textarea' contentEditable></div>
               </div>
             </div>
             <div className='row'>
