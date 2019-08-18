d1101181a8a54aedd52afdcd406a8c8db76d49f8
tjamesmac <tjamesmac@gmail.com>
2019-08-18 09:06:25 -0700

    Add new prop to component test

--- src/public/Typescript/Modal/Modal.test.tsx
+++ src/public/Typescript/Modal/Modal.test.tsx
@@ -5,8 +5,9 @@ import Modal from './Modal';
 describe('Main page component', () => {
 
   it('it renders correctly', () => {
+    const modalPosition = {top: 1, left: 2};
     const tree = renderer
-      .create(<Modal words={['one', 'two']} />)
+      .create(<Modal words={['one', 'two']} position={modalPosition} />)
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
