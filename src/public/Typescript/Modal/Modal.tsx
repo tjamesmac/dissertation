fece844ce3cbb310e3d529ef09623f84e1551732
tjamesmac <tjamesmac@gmail.com>
2019-08-18 09:06:11 -0700

    Add modal positioning prop

--- src/public/Typescript/Modal/Modal.tsx
+++ src/public/Typescript/Modal/Modal.tsx
@@ -1,15 +1,27 @@
 import * as React from 'react';
 
-interface IModalProps { words: string[]; }
+export interface IModalPosition {
+  top: number;
+  left: number;
+}
+interface IModalProps {
+  words: string[];
+  position: IModalPosition;
+}
 
 const Modal = (prop: IModalProps): JSX.Element => {
 
   const words: string[] = prop.words;
+  const position = prop.position;
+  const positionCSS = {
+    top: prop.position.top,
+    left: prop.position.left,
+  };
   const wordMap = words.map((word: string, index: number) => {
     return <li key={index}>{word}</li>;
   });
   return (
-    <ul className='synonym'>
+    <ul className='synonym' style={positionCSS}>
       {wordMap}
     </ul>
   );
