c4dea1708573483128a557e80918fb17ef817393
tjamesmac <tjamesmac@gmail.com>
2019-08-19 14:00:26 -0700

    Add dummy modal data

--- src/public/Typescript/Modal/Modal.test.tsx
+++ src/public/Typescript/Modal/Modal.test.tsx
@@ -6,9 +6,10 @@ describe('Main page component', () => {
 
   it('it renders correctly', () => {
     const modalPosition = {top: 1, left: 2};
+    const modalWords = { word: 'hello', synonyms: ['hi', 'hiya']};
     const tree = renderer
       .create(<Modal
-                words={['one', 'two']}
+                words={modalWords}
                 position={modalPosition}
                 onWordClick={() => console.log('hello')}
               />,
