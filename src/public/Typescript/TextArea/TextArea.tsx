cdb700ab78823c2f8c0230e0fb64b09397b0dd53
tjamesmac <tjamesmac@gmail.com>
2019-08-18 18:06:26 -0700

    Sketch out prop to be

--- src/public/Typescript/TextArea/TextArea.tsx
+++ src/public/Typescript/TextArea/TextArea.tsx
@@ -4,7 +4,16 @@ export interface ITextArea {
   children?: JSX.Element[] | JSX.Element;
 }
 
+/**
+ * 
+ * I am going to add the response and function into this so I don't need a helper
+ * function
+ */
 export const TextArea = ( prop: ITextArea ): JSX.Element => {
+  const response = prop.response;
+  if (response) {
+    console.log(response);
+  }
 
   return (
     <div id='textarea' contentEditable>
