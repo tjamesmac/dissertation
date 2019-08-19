c7e63acb4fce2869e657f536238811c93e181324
tjamesmac <tjamesmac@gmail.com>
2019-08-19 14:28:00 -0700

    refactor in progress

--- src/public/Typescript/Main/helpers.tsx
+++ src/public/Typescript/Main/helpers.tsx
@@ -8,7 +8,6 @@ export interface IResponse {
 
 export const validateWords = ( response: IResponse[] ): string => {
 
-  
   const textAreaValue: string =
     (document.getElementById('textarea') as HTMLDivElement)
     .innerText;
@@ -29,3 +28,46 @@ export const validateWords = ( response: IResponse[] ): string => {
   }
   return toChange + '&#8203;';
 };
+
+// export const highlightWords = ( response: IResponse[] ) => {
+
+//   const textareaValue = (document.getElementById('textarea') as HTMLDivElement);
+//   const textareaString = textareaValue.innerText;
+//   const splitText = textareaString.split(' ');
+  
+
+//   while (textareaValue.firstChild) { textareaValue.removeChild(textareaValue.firstChild); }
+//   for (let key of response) {
+//     if (textareaValue.innerText.includes(key.word)) {
+//       // turns green
+//       const span = createSpan('span', key.word);
+//       span.style.color = 'green';
+//       textareaValue.appendChild(span);
+//     } else {
+//       const textNode = document.createTextNode()
+//     }
+//   }
+//   for (let element of splitInitial) {
+//     if (synonyms) {
+//       if (element === synonyms.word) {
+//         const span = document.createElement('span');
+//         span.style.color = 'blue';
+//         const text = document.createTextNode(value);
+//         span.appendChild(text);
+//         initial.appendChild(span);
+//       } else {
+//         const textNode = document.createTextNode(element);
+//         initial.appendChild(textNode);
+//       }
+//     }
+//   }
+// }
+
+// const createSpan = ( element: string, text: string ) => {
+
+//   const span = document.createElement(element);
+//   const textNode = document.createTextNode(text);
+//   span.appendChild(textNode);
+//   return span;
+
+// };
