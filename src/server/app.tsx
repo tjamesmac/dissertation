bbf5cb396ab6c9ad1964fe6ae0b88a117ba838a5
tjamesmac <tjamesmac@gmail.com>
2019-08-20 14:01:37 -0700

    Add port env

--- src/server/app.tsx
+++ src/server/app.tsx
@@ -2,7 +2,7 @@
 import Server from './server';
 
 const app = new Server().app;
-const PORT: number = 3000;
+const PORT: number = 3000 || process.env.PORT;
 
 app.listen( PORT, () => {
   console.log( `Express server listening on port ${PORT}` );
