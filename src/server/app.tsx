05b1c8465adc88c6ce1ff5019feb197f8a316b98
tjamesmac <tjamesmac@gmail.com>
2019-08-20 14:08:12 -0700

    Update port

--- src/server/app.tsx
+++ src/server/app.tsx
@@ -2,7 +2,7 @@
 import Server from './server';
 
 const app = new Server().app;
-const PORT: number = 3000 || process.env.PORT;
+const PORT: any = process.env.PORT || 3000;
 
 app.listen( PORT, () => {
   console.log( `Express server listening on port ${PORT}` );
