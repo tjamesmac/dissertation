4ae9ecd8d8457ae2b657c84058c25b9e6d8d666a
tjamesmac <tjamesmac@gmail.com>
2019-08-20 15:34:04 -0700

    correct names

--- src/public/type/App/App.test.tsx
+++ src/public/type/App/App.test.tsx
@@ -1,7 +1,7 @@
 import { shallow } from 'enzyme';
 import * as React from 'react';
 import renderer from 'react-test-renderer';
-import { App } from '../App/App';
+import { App } from './App';
 
 describe('App component renders correctly', () => {
   it('renders as expected', () => {
