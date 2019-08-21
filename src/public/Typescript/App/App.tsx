c0ae98e0fc532e95fa189bb33b58eee313f90f5e
tjamesmac <tjamesmac@gmail.com>
2019-08-20 19:16:01 -0700

    Add heading

--- src/public/Typescript/App/App.tsx
+++ src/public/Typescript/App/App.tsx
@@ -1,9 +1,17 @@
 import * as React from 'react';
-// import { Hello } from '../Hello/Hello';
 import { Main } from '../Main/Main';
 
 export const App = () => {
-  return(
-    <Main />
+  return (
+    <div>
+    <div className='container'>
+      <div className='row'>
+        <div className='col-12'>
+          <h1>Gender Neutral</h1>
+        </div>
+      </div>
+    </div>
+      <Main />
+    </div>
   );
 };
