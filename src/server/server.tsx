7101058eff99e80135d1f5dbd520b8f6d6affb3b
tjamesmac <tjamesmac@gmail.com>
2019-08-21 09:40:32 -0700

    Fix import order

--- src/server/server.tsx
+++ src/server/server.tsx
@@ -5,8 +5,8 @@ import * as path from 'path';
 import React from 'react';
 import { renderToString } from 'react-dom/server';
 import { App } from '../public/Typescript/App/App';
-import { htmlTemplate } from './htmlTemplate';
 import { getWordData, isString  } from './helpers/wordProcessing';
+import { htmlTemplate } from './htmlTemplate';
 
 class Server {
   public app: express.Application;
