3424eec252e8febd49c1b3490d99484e523cd651
tjamesmac <tjamesmac@gmail.com>
2019-08-22 11:50:16 -0700

    Add interface file

---
+++ src/public/Typescript/Main/Main.interface.tsx
@@ -0,0 +1,14 @@
+export interface IWordAndSynonym {
+  word: string;
+  synonyms: string[];
+};
+
+/**
+ * THIS IS A TEST INTERFACE
+ */
+export interface IDataPL {
+  originalString: string;
+  newString: string;
+  orderOfWords: string[];
+  demographic: string;
+};
