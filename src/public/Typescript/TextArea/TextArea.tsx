17045c37e3d70997a24bea8510f4ca94de51ba98
tjamesmac <tjamesmac@gmail.com>
2019-08-22 11:50:53 -0700

    Update imports and remove interface

--- src/public/Typescript/TextArea/TextArea.tsx
+++ src/public/Typescript/TextArea/TextArea.tsx
@@ -1,18 +1,7 @@
 import * as React from 'react';
-import { IResponse, validateWords } from '../Main/helpers';
+import { ITextArea } from '../textArea/textArea.interface';
 
-export interface ITextArea {
-  children?: JSX.Element[] | JSX.Element;
-  response: IResponse[] | null;
-}
-
-/**
- * 
- * I am going to add the response and function into this so I don't need a helper
- * function
- */
 export const TextArea = ( prop: ITextArea ): JSX.Element => {
-
   return (
     <div id='textarea' contentEditable>
       { prop.children }
