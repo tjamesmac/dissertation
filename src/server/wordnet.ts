17dc4f47f5605f1de1aec5161323e669d1a0be79
tjamesmac <tjamesmac@gmail.com>
2019-08-24 18:26:29 -0700

    Houston we have lift off

--- src/server/wordnet.ts
+++ src/server/wordnet.ts
@@ -3,7 +3,6 @@ import WordPos from 'wordpos';
 const wordpos = new WordPos();
 
 export async function overAll() {
-  console.log('i am working');
   const testString = 'this is my big strong string';
 
   const getAdjective = async ( string: string ) => {
@@ -20,43 +19,21 @@ export async function overAll() {
   const lookupA = async ( array: string[] ) => {
     const wordArray = array;
     const obj: any = {};
-    // wordArray.forEach( async ( result ) => {
-    //   // obj[result] = result;
-    //   const getAdjData = wordpos.lookupAdjective( result, ( word: any ) => {
-    //     // console.log(result)
-    //     const object: any = {};
-    //     object[result] = [];
-    //     for ( let element of word ) {
-    //       if ( element.pos === 'a' || element.pos === 's' ) {
-    //         console.log(element.pos, result);
-    //         const pos = element.pos;
-    //         object[result] = [ ...object[result], ...pos ];
-    //       }
-    //     }
-    //     Object.assign(obj, object);
-    //     console.log(obj);
-    //     return obj;
-    //   })
-    //   .then((res: any) => {
-    //     console.log(res);
-    //   })
-    //   // this returns everything
-    //   // console.log( await getAdjData, 'data' );
-    //   console.log('am here');
-    //   return getAdjData;
-    // } );
-    wordArray.forEach( ( word ) => {
+
+    for (const word of wordArray) {
       console.log(word);
-    });
-    obj.awesome = [];
-    await wordpos.lookupAdjective('big', ( response: any ) => {
-      for ( let element of response ) {
-        console.log(element.pos);
-        obj.awesome = [ ...obj.awesome, element.pos ];
-      }
-      // obj.awesome = response;
-    });
+      obj[word] = [];
+      console.log(word);
+      await wordpos.lookupAdjective( word , ( response: any ) => {
+        for ( let element of response ) {
+          console.log(element.pos);
+          const pos = element.synonyms;
+          obj[word] = [ ...obj[word], ...pos ];
+        }
+      });
+    }
     return obj;
+
   };
   return await lookupA( await confirmAdjective );
 }
