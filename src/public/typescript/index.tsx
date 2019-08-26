c4e7d9d5f86241cba0936ff992794674010052cc
tjamesmac <tjamesmac@gmail.com>
2019-08-26 13:38:50 -0700

    update

--- src/public/typescript/index.tsx
+++ src/public/typescript/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './app/pp';
+import App from './app/app';
 
 ReactDOM.hydrate(<App/>, document.getElementById('app'));
