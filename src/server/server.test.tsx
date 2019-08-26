520c1ad07e7a50f6fb79b37988db189e7cd2500c
tjamesmac <tjamesmac@gmail.com>
2019-08-26 14:05:09 -0700

    udpate

--- src/server/server.test.tsx
+++ src/server/server.test.tsx
@@ -4,7 +4,7 @@ import Server from './server';
 
 import React from 'react';
 import { renderToString } from 'react-dom/server';
-import { Hello } from '../public/typescript/hello/Hello';
+import { Hello } from '../public/typescript/hello/hello';
 import { htmlTemplate } from './htmlTemplate';
 
 describe('my server', () => {
