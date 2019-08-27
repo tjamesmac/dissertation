2bc54360fe6fda63d95977de5db135bb1506f3b3
tjamesmac <tjamesmac@gmail.com>
2019-08-27 14:45:30 -0700

    Add fetch

--- src/public/typescript/choice/choice.tsx
+++ src/public/typescript/choice/choice.tsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const Choice: React.FunctionComponent = () => {
+
+  const fetchData = async () => {
+    try {
+      const URL = '/choice';
+      const data = await fetch(URL);
+      const response = await data;
+      if ( response.status === 200 ) {
+        console.log('got it');
+        const responseJSON = await response.json();
+        console.log(responseJSON);
+      }
+    } catch (error) {
+      console.error('fetch results', error);
+    }
+  };
+  useEffect( () => {
+      console.log('lets effect');
+      fetchData();
+  });
+
   return (
       <div className='row'>
         <div className='col-12'>
           <h2 className='header'>Options!</h2>
           <p>Please pick one of the two options below.</p>
+          <button className='btn btn-primary' onClick={() => fetchData()}>Click</button>
         </div>
       </div>
   );
