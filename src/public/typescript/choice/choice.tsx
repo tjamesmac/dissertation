d03c344fa114e377b741ae68d56fa399e5b92460
tjamesmac <tjamesmac@gmail.com>
2019-08-27 13:30:11 -0700

    Add choice

---
+++ src/public/typescript/choice/choice.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Choice: React.FunctionComponent = () => {
+  return (
+      <div className='row'>
+        <div className='col-12'>
+          <h2 className='header'>Options!</h2>
+          <p>Please pick one of the two options below.</p>
+        </div>
+      </div>
+  );
+};
+
+export default Choice;
