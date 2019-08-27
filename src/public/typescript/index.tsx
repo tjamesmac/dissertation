defe7cb59fde4b0fe6373a70e8bc2ca764aebd03
tjamesmac <tjamesmac@gmail.com>
2019-08-27 12:03:01 -0700

    Add BrowserRouter to root of App

--- src/public/typescript/index.tsx
+++ src/public/typescript/index.tsx
@@ -1,5 +1,11 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './app/app';
 
-ReactDOM.hydrate(<App/>, document.getElementById('app'));
+ReactDOM.hydrate(
+  <BrowserRouter>
+    <App/>
+  </BrowserRouter>
+  ,
+  document.getElementById('app'));
