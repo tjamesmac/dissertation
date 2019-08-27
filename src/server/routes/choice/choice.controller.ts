58833abf8df81f061df168d927ceafeda7a55d14
tjamesmac <tjamesmac@gmail.com>
2019-08-27 14:45:04 -0700

    Add values

--- src/server/routes/choice/choice.controller.ts
+++ src/server/routes/choice/choice.controller.ts
@@ -2,7 +2,11 @@ import { Request, Response } from 'express';
 
 const controller = {
   getNew: ( req: Request, res: Response ) => {
-    return res.send('hello');
+    console.log('hello why are you not working');
+    const object = { value: 'hello' };
+    const json = JSON.stringify(object);
+    console.log(json);
+    return res.send(object);
   },
   postName: ( req: Request, res: Response ) => {
     return res.send('poop');
