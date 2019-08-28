4e5f1f5fcf99b8afe7b52eddbd7ed0c1675c4302
tjamesmac <tjamesmac@gmail.com>
2019-08-28 12:40:39 -0700

    Add choice response

--- src/public/typescript/choice/choice.tsx
+++ src/public/typescript/choice/choice.tsx
@@ -1,31 +1,62 @@
 import React, { useEffect } from 'react';
 
+interface IOption {
+  data: string;
+  demo: string;
+  // key: string;
+}
+const Option = ( prop: IOption ) => {
+  const data = prop.data;
+  const demo = prop.demo;
+  return (
+    <div className='option' data-demo={ demo }>
+      <p>{data}</p>
+    </div>
+  );
+};
+
 const Choice: React.FunctionComponent = () => {
+
+  const [ choices, setChoices ] = React.useState();
 
   const fetchData = async () => {
     try {
+
       const URL = '/choice/data';
       const data = await fetch(URL);
       const response = await data;
+
       if ( response.status === 200 ) {
-        console.log('got it');
         const responseJSON = await response.json();
         console.log(responseJSON);
+        setChoices(responseJSON);
       }
     } catch (error) {
       console.error('fetch results', error);
     }
   };
   useEffect( () => {
-      console.log('lets effect');
       fetchData();
-  });
+  }, []);
+  let optionMap;
+  if (choices) {
+    optionMap = choices.map( ( options: any ) => {
+      return (
+        <Option
+          key={ options._id }
+          data={ options.originalString }
+          demo={ options.demographic }
+        />
+      );
+    } );
+  }
 
   return (
       <div className='row'>
         <div className='col-12'>
           <h2 className='header'>Options!</h2>
           <p>Please pick one of the two options below.</p>
+          {optionMap}
           <button className='btn btn-primary' onClick={() => fetchData()}>Click</button>
         </div>
       </div>
