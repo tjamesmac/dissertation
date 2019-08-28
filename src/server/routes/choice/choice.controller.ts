24e604a0890b978d22368a3859179d5fec66f044
tjamesmac <tjamesmac@gmail.com>
2019-08-28 16:00:08 -0700

    Add limit to results

--- src/server/routes/choice/choice.controller.ts
+++ src/server/routes/choice/choice.controller.ts
@@ -9,14 +9,15 @@ const controller = {
     // next step is to then check if there are two from each demographic;
     const length = await models.Data.countDocuments();
 
-    if (length > 2) {
+    if (length > 3) {
       const data = models.Data.find( (error, response) => {
         if (error) {
           console.error('finding documents', error);
         }
+        console.log(response);
         return res.send(response);
 
-      }).sort({ _id: -1 }).limit(2);
+      }).sort({ _id: -1 }).limit(3);
     }
   },
   postName: ( req: Request, res: Response ) => {
