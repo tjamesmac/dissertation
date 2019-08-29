25b1974501569b9903d4a830af2242347f694159
tjamesmac <tjamesmac@gmail.com>
2019-08-29 08:40:46 -0700

    Add id

--- src/public/typescript/choice/option.extra.tsx
+++ src/public/typescript/choice/option.extra.tsx
@@ -5,9 +5,11 @@ import { IOption } from './choice.interface';
 const Option = ( prop: IOption ) => {
   const data = prop.data;
   const demo = prop.demo;
+  const id = prop.id;
   const handleClick = prop.onClick;
   return (
     <div
+      id={ id }
       onClick={ handleClick }
       className='option'
       data-demo={ demo }
