2f094e9440b1b3a9acfb42433ba36ed6f77ff080
tjamesmac <tjamesmac@gmail.com>
2019-09-01 13:44:01 -0700

    Add admin page

---
+++ src/public/typescript/admin/admin.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export const Admin = ( ): JSX.Element => {
+  return (
+    <div className='info'>
+      <h2>This is my admin page that will eventually reveal my results.</h2>
+    </div>
+  );
+};
+
+export default Admin;
