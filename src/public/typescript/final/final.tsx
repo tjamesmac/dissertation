240ce9531c13a65d0a549250c35160ab5716f826
tjamesmac <tjamesmac@gmail.com>
2019-09-01 13:44:17 -0700

    Add final

---
+++ src/public/typescript/final/final.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export const Final = ( ): JSX.Element => {
+  return (
+    <div className='info'>
+      <p>Thank you for taking part.</p>
+    </div>
+  );
+};
+
+export default Final;
