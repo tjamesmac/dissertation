e68873d8d5cb0de986cc22efe7f048161cede509
tjamesmac <tjamesmac@gmail.com>
2019-09-02 10:05:57 -0700

    Update final page

--- src/public/typescript/final/final.tsx
+++ src/public/typescript/final/final.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 
 export const Final = ( ): JSX.Element => {
   return (
     <div className='info'>
       <p>Thank you for taking part.</p>
+      <Link to='/' className='btn btn-primary'>Back to the beginning</Link>
     </div>
   );
 };
