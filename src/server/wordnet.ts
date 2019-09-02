02c718c0cbc545b712f29cd53374d8b8de5f729a
tjamesmac <tjamesmac@gmail.com>
2019-09-01 21:00:42 -0700

    Remove additional syns

--- src/server/wordnet.ts
+++ src/server/wordnet.ts
@@ -24,7 +24,7 @@ export async function overAll( inputString: string ) {
       await wordpos.lookupAdjective( word , ( response: any ) => {
         for ( const element of response ) {
           const synonyms = element.synonyms;
-          if (element.pos === 'a' || element.pos === 's') {
+          if (element.pos === 'a') {
             for (const w of synonyms) {
               if (word !== w) {
                 if (!obj[word].includes(w)) {
