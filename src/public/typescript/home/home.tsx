9e79c02189f6306f36fc0bb32de9cfad8f13602e
tjamesmac <tjamesmac@gmail.com>
2019-09-01 20:49:30 -0700

    Fix button

--- src/public/typescript/home/home.tsx
+++ src/public/typescript/home/home.tsx
@@ -6,27 +6,29 @@ export const Home: React.FunctionComponent = () => {
       <div className='row'>
         <div className='col-12'>
           <h2>Welcome!</h2>
-          <p>
-            This little program has been built to
-             help see whether an advert is male or
-             female biased.
-          </p>
-          <p>
-            After clicking on the start button below,
-             you will be presented with a text input area.
-          </p>
-          <p>
-            You will be able to copy and paste text into
-             the input area. After doing so, any adjectives
-             that can be changed will be highlighted in green.
-             Hovering over them will allow you to change the word.
-          </p>
-          <p>
-            When you have submitted the changes to your advert, you will
-             be able to choose from two previous posted adverts.
-          </p>
-        </div>
+          <div className='info'>
+            <p>
+              This little program has been built to
+              help see whether an advert is male or
+              female biased.
+            </p>
+            <p>
+              After clicking on the start button below,
+              you will be presented with a text input area.
+            </p>
+            <p>
+              You will be able to copy and paste text into
+              the input area. After doing so, any adjectives
+              that can be changed will be highlighted in green.
+              Hovering over them will allow you to change the word.
+            </p>
+            <p>
+              When you have submitted the changes to your advert, you will
+              be able to choose from two previous posted adverts.
+            </p>
+          </div>
         <Link to='/main' className='btn btn-primary'>Start</Link>
+        </div>
       </div>
   );
 };
