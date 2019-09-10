c23ccc3fd1c2a495d6fbf53e6dd3d224de6ae987
tjamesmac <tjamesmac@gmail.com>
2019-09-09 19:31:33 -0700

    Make use of alternative wordnet

--- src/server/routes/home/home.controller.tsx
+++ src/server/routes/home/home.controller.tsx
@@ -3,24 +3,29 @@ import React from 'react';
 import { renderToString } from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom';
 import App from '../../../public/typescript/app/app';
+import { overAll } from '../../helpers/wordnetTwo';
 import { htmlTemplate } from '../../htmlTemplate';
-import { overAll } from '../../wordnet';
+// import { overAll } from '../../wordnet';
 
 const controller = {
   postHomeController: async ( req: Request, res: Response ) => {
-    // console.log('inside everything');
     const words = req.body;
     // this needs to be lowercased before going through the function
 
+    // overAll( words.value )
+    //   .then( ( answer ) => {
+    //     for ( const prop of Object.keys(answer) ) {
+    //       if (!answer[prop].length) {
+    //         delete answer[prop];
+    //       }
+    //     }
+    //     return res.send( answer );
+    //   } );
     overAll( words.value )
-      .then( ( answer ) => {
-        for ( const prop of Object.keys(answer) ) {
-          if (!answer[prop].length) {
-            delete answer[prop];
-          }
-        }
-        return res.send( answer );
-      } );
+    .then( ( answer ) => {
+      // console.log(answer);
+      return res.send( answer );
+    } );
   },
   getSSR: ( req: Request, res: Response ) => {
     const context = { };
