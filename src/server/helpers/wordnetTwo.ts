b975d07311181833edef0abd21e94c8d7836be19
tjamesmac <tjamesmac@gmail.com>
2019-09-09 19:31:16 -0700

    Return list of words by pos

--- src/server/helpers/wordnetTwo.ts
+++ src/server/helpers/wordnetTwo.ts
@@ -2,11 +2,8 @@ import WordPos from 'wordpos';
 
 const wordpos = new WordPos();
 
-// right now all i need to do is loop through that array and construct an object with all the possibilities
-
 export async function overAll( inputString: string ) {
   const testString = inputString;
-
   const getPartOfSpeech = async ( string: string ) => {
     const words = string;
 
@@ -15,14 +12,8 @@ export async function overAll( inputString: string ) {
     } );
 
   };
-  // return getPartOfSpeech(testString);
-  const confirmPOS = await getPartOfSpeech(testString);
-  // console.log(confirmPOS.nouns);
-  // console.log(confirmPOS.verbs);
-  // console.log(confirmPOS.adjectives);
-  // console.log(confirmPOS.adverbs);
+  const confirmPOS: any = await getPartOfSpeech(testString);
 
-  const obj: any = {};
   const returnPOS = (partOfSpeech: string) => {
     switch ( partOfSpeech ) {
       case 'verbs':
@@ -39,53 +30,70 @@ export async function overAll( inputString: string ) {
         throw new Error();
     }
   };
+  const turnBackPOS = (partOfSpeech: string) => {
+    switch ( partOfSpeech ) {
+      case 'v':
+        return 'verbs';
+      case 'a':
+      case 's':
+        return 'adjectives';
+      case 'r':
+        return 'adverbs';
+      case 'n':
+        return 'nouns';
+      case 'rest':
+        return 'rest';
+      default:
+        throw new Error();
+    }
+  };
 
-  const lookupA = async ( array: string[], partOfSpeech: string ) => {
-    const wordArray = array;
-    const tempObj: any = {};
-    console.log(partOfSpeech);
-    console.log(returnPOS(partOfSpeech), 'this is the part of speech i need');
-    const initialOfPOS = returnPOS(partOfSpeech);
-    console.log(initialOfPOS);
+  const lookupWord = async ( words: string[], partOfSpeech: string ) => {
+    const pos = returnPOS(partOfSpeech);
+    const tempObject: any = {};
+    for ( const word of words ) {
+      tempObject[word] = [];
 
-    // this was for each but await doesn't work with it
-    for (const word of wordArray) {
-      tempObj[word] = [];
-      tempObj[word][initialOfPOS] = [];
-      await wordpos.lookup( word , ( response: any ) => {
-        // console.log(word, 'this is called my word');
-        console.log(response);
+      await wordpos.lookup( word, ( response: any ) => {
+
         for ( const element of response ) {
           const synonyms = element.synonyms;
-          const speech = element.pos;
-          
-          console.log(speech, 'this is the speech part that I need to separate objects');
-          console.log(synonyms);
-          if (initialOfPOS === speech) {
-          for ( const w of synonyms ) {
-            if ( word !== w ) {
-              if (!tempObj[word][initialOfPOS].includes(w)) {
+          const elementPOS = element.pos;
+          if ( turnBackPOS(elementPOS) === turnBackPOS(pos) ) {
 
-                tempObj[word][initialOfPOS] = [ ...tempObj[word][initialOfPOS], ...[w] ];
+            for ( const w of synonyms ) {
+              if ( word !== w ) {
+                if (!tempObject[word].includes(w) && tempObject[word].length < 5) {
+
+                  tempObject[word] = [ ...tempObject[word], ...[w] ];
+                }
               }
             }
           }
+
         }
-        }
-      });
+      } );
     }
-    console.log(tempObj);
-    return tempObj;
+    console.log(tempObject);
+    return tempObject;
   };
-  for ( const pos of Object.keys(confirmPOS) ) {
-    
-    const partOf = confirmPOS[pos];
-    const lookUpResult = await lookupA(await partOf, pos);
-    // console.log(lookUpResult);
-    obj[pos] = lookUpResult;
-    // console.log('onto the next one', pos);
+
+  const partOfSpeechKeys = Object.keys( await confirmPOS );
+
+  const object: any = {};
+
+  for ( const posKey of partOfSpeechKeys ) {
+    object[posKey] = [];
+
+    if (confirmPOS[posKey].length) {
+
+      const lookupArray = await confirmPOS[posKey];
+
+      const lookUpResult = await lookupWord( lookupArray, posKey);
+
+      object[posKey] = await lookUpResult;
+    }
   }
-  console.log(obj, 'this is my result object');
-  return obj;
-  
+  console.log(object, 'final object');
+  return object;
 }
