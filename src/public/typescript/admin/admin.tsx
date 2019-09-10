e4b87fad2552f3b0a3e5fe8fc2248f5f0d3459aa
tjamesmac <tjamesmac@gmail.com>
2019-09-10 16:43:04 -0700

    Add to admin

--- src/public/typescript/admin/admin.tsx
+++ src/public/typescript/admin/admin.tsx
@@ -1,9 +1,52 @@
 import * as React from 'react';
+import Result, { ResultData, ResultFinal } from './result';
 
 export const Admin = ( ): JSX.Element => {
+  const [ words, setWords ] = React.useState();
+  const [ final, setFinal ] = React.useState();
+  const [ data, setData ] = React.useState();
+  const fetchData = async () => {
+    try {
+
+      const URL = '/admin/data';
+      const data = await fetch(URL);
+      const response = await data;
+
+      if (response.status === 200) {
+        console.log('I have finally made it here');
+        const responseJSON = await response.json();
+        console.log(responseJSON);
+        setWords(responseJSON.words);
+        setFinal(responseJSON.final);
+        setData(responseJSON.data);
+      }
+    } catch (error) {
+      console.error('fetch results', error);
+    }
+  };
+  React.useEffect( () => {
+      fetchData();
+  }, []);
+  let dataComp;
+  if (data) {
+    
+    dataComp = <Result data={ data } component={ <ResultData/> } />;
+      
+  } else {
+    dataComp = <div>Loading data...</div>;
+  }
+  let finalComp;
+  if (data) {
+    
+    finalComp = <Result data={ data } component={ <ResultFinal/> } />;
+  } else {
+    finalComp = <div>Loading data...</div>;
+  }
   return (
     <div className='info'>
       <h2>This is my admin page that will eventually reveal my results.</h2>
+      {dataComp}
+      {finalComp}
     </div>
   );
 };
