4e61d5d764d06b79ce8476fa28e05b6044d8d4fa
tjamesmac <tjamesmac@gmail.com>
2019-09-10 16:45:45 -0700

    correct controller

--- src/server/routes/data/data.controller.ts
+++ src/server/routes/data/data.controller.ts
@@ -5,19 +5,18 @@ import models from '../../models/index';
 const dataController = ( req: Request, res: Response ) => {
 
   const body = req.body;
-  console.log(body);
+
   const data = new models.Data();
   data.originalString = body.originalString;
   data.newString = body.newString;
   data.orderOfWords = body.orderOfWords;
-  data.initialGenderedWords = body.initialGenderedWords;
+  data.initialGenderedWords = body.initialGenderWords;
   data.finalGenderedWords = genderCheck(body.orderOfWords);
   data.demographic = body.demographic;
   data.length = body.length;
   data.save( (error: string) => {
     if (error) { console.log(error); }
     console.log('data saved');
-    console.log(data, 'this is my data object that i am saving');
   } );
 
   return res.sendStatus(200);
