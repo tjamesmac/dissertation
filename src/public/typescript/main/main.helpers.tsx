e85b9c6e479174bcbdc8707bc4be3ef88775aa43
tjamesmac <tjamesmac@gmail.com>
2019-09-09 19:30:55 -0700

    Make text green again

--- src/public/typescript/main/main.helpers.tsx
+++ src/public/typescript/main/main.helpers.tsx
@@ -156,50 +156,108 @@ export const dataReducer = ( state: IDataPL, action: any ) => {
 //   }
 //   return toChange + '&#8203;';
 // };
-const wordMatcher = (key: string, stringToChange: string, gender: string) => {
-  
+const wordMatcher = (key: string, stringToChange: string) => {
+  console.log(key);
   const word = key;
   const regex = new RegExp('\\b' + key + '\\b', 'g');
-  const newString = stringToChange.replace(regex, `<span class='${gender}'>${word}</span>`);
-
+  const newString = stringToChange.replace(regex, `<span class=''>${word}</span>`);
+  console.log(newString, 'newString');
   return newString;
 };
 
-export const validateWords = ( response: any, textCheck: string ): any => {
-  const maleWords = [];
-  const femaleWords = [];
-  const keys = Object.keys(response);
-  const newKeys = keys.filter( (x) => {
-    if (response[x].length) {
-      return x;
-    } else {
-      return null;
-    }
-  } );
-  let toChange: string = textCheck;
-  for (const key of newKeys) {
-      for (const male of genderedWords.maleGenderedWords) {
-        // console.log(key, word);
-        // console.log(word.search(key));
-        if (textCheck.includes(key) && key.match(male)) {
+// export const validateWords = ( response: any, textCheck: string ): any => {
+//   const maleWords = [];
+//   const femaleWords = [];
+//   const keys = Object.keys(response);
+//   console.log(keys);
+//   const newKeys = keys.filter( (x) => {
+//     if (response[x].length) {
+//       return x;
+//     } else {
+//       return null;
+//     }
+//   } );
+//   console.log(newKeys, 'new keys');
+//   let toChange: string = textCheck;
+//   for (const key of newKeys) {
+//       for (const male of genderedWords.maleGenderedWords) {
+//         // console.log(key, word);
+//         // console.log(word.search(key));
+//         if (textCheck.includes(key) && key.match(male)) {
 
-          const maleNew  = wordMatcher(key, toChange, 'male');
-          maleWords.push(key);
-          toChange = maleNew;
-        }
-      }
-      for (const female of genderedWords.femaleGenderedWords) {
-        if (textCheck.includes(key) && key.match(female)) {
-          femaleWords.push(key);
-          const femaleWord = wordMatcher(key, toChange, 'female');
-          toChange = femaleWord;
-        }
-      }
+//           const maleNew  = wordMatcher(key, toChange, 'male');
+//           maleWords.push(key);
+//           toChange = maleNew;
+//         }
+//       }
+//       for (const female of genderedWords.femaleGenderedWords) {
+//         if (textCheck.includes(key) && key.match(female)) {
+//           femaleWords.push(key);
+//           const femaleWord = wordMatcher(key, toChange, 'female');
+//           toChange = femaleWord;
+//         }
+//       }
+//   }
+//   toChange += '&#8203;';
+//   const returnedObject = { male: maleWords, female: femaleWords, textChange: toChange };
+//   return returnedObject;
+// };
+
+export const validateWords = ( response: any, textCheck: string ): any => {
+  const totalWords: any = [];
+  const keys = Object.keys(response);
+
+  const availableWords = [];
+  for ( const pos of keys ) {
+
+    const wordKeys = Object.keys(response[pos]);
+
+    availableWords.push(wordKeys);
   }
+
+  const flattened = availableWords
+    .reduce( (accumulator, currentValue) => {
+      return accumulator.concat(currentValue);
+  });
+  const validWords: string[] = [];
+  let toChange: string = textCheck;
+  for (const word of flattened) {
+
+    if ( !validWords.includes( word ) ) {
+      validWords.push(word);
+      const newWord = wordMatcher(word, toChange);
+      toChange = newWord;
+    }
+  }
+
+  // THIS IS PART OF THE OBJECT I WANT TO SEND
+  console.log(validWords);
+
+  // for ( const key of keys ) {
+  //   if (Object.entries(response[key]).length !== 0 ) {
+  //     // console.log(response[key], 'key', key);
+  //     const innerKeys = Object.keys(response[key]);
+  //     for (const innerKey of innerKeys) {
+  //       if (!totalWords.includes(innerKey)) {
+  //         totalWords.push(innerKey);
+  //       }
+  //     }
+  //     // console.log(innerKeys, 'these are the inner keys I want');
+  //     for ( const word of innerKeys) {
+  //       const wordObject = response[key];
+  //       if (wordObject[word].length) {
+  //         // console.log(wordObject[word], 'inside the word synonym array');
+  //       }
+  //     }
+  //   }
+  // }
+  // console.log(totalWords);
   toChange += '&#8203;';
-  const returnedObject = { male: maleWords, female: femaleWords, textChange: toChange };
-  return returnedObject;
+  const validObject = { updatedString: toChange };
+  
+  return validObject;
 };
+
 
 export const greenify = async () => {
   const textarea = (document.getElementById('textarea') as HTMLDivElement);
