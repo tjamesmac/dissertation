a1743bd826a4c5d0398f76d4fbbc6914aaf9b69a
tjamesmac <tjamesmac@gmail.com>
2019-09-10 09:04:46 -0700

    Update modal component

--- src/public/typescript/modal/modal.tsx
+++ src/public/typescript/modal/modal.tsx
@@ -15,26 +15,111 @@ interface IModalProps {
 }
 
 const Modal = ( prop: IModalProps ): JSX.Element => {
-
-  const words = prop.words;
+  const words: any = prop.words;
+  console.log(words, 'words');
+  const wordKeys = Object.keys(words);
+  console.log(wordKeys);
+  const adverbs = [];
+  const adjectives = [];
+  const verbs = [];
+  const nouns = [];
+  for ( const type of wordKeys ) {
+    if (type !== 'word') {
+      console.log(type);
+      console.log(words[type]);
+      if (type === 'adjectives') {
+        adjectives.push(words[type]);
+      }
+      if (type === 'adverbs') {
+        adverbs.push(words[type]);
+      }
+      if (type === 'nouns') {
+        nouns.push(words[type]);
+      }
+      if (type === 'verbs') {
+        verbs.push(words[type]);
+      }
+    }
+  }
   const hover = prop.hover;
   const removeHover = prop.removeHover;
-  const synonyms = words.synonyms;
   const position = prop.position;
   const positionCSS = {
     top: position.top,
     left: position.left,
   };
-  const wordMap = synonyms.map(( word: string, index: number ) => {
+  let wordMapAdj;
+  let adjType;
+  if (adjectives.length) {
+    adjType = <li className='synonyms-type'>Adjectives</li>;
+    const actualAdj = adjectives[0];
+    wordMapAdj = actualAdj.map(( word: string, index: number ) => {
+      return <li
+                className='synonyms-item'
+                onClick={ (e) => prop.onWordClick(e) }
+                key={index}
+              >
+                {word}
+              </li>;
+    });
+  }
+  let wordMapAdverb;
+  let adverbType;
+  if (adverbs.length) {
+    adverbType = <li className='synonyms-type'>Adverbs</li>;
+    const actualAdverbs = adverbs[0];
+    
+    wordMapAdverb = actualAdverbs.map(( word: string, index: number ) => {
     return <li
               className='synonyms-item'
-              onClick={(e) => prop.onWordClick(e)}
-              key={index}>{word}
+              onClick={ (e) => prop.onWordClick(e) }
+              key={index}
+            >
+              {word}
             </li>;
   });
+  }
+  let wordMapNouns;
+  let nounType;
+  if (nouns.length) {
+    nounType = <li className='synonyms-type'>Nouns</li>;
+    const actualNouns = nouns[0];
+    wordMapNouns = actualNouns.map(( word: string, index: number ) => {
+      return <li
+                className='synonyms-item'
+                onClick={ (e) => prop.onWordClick(e) }
+                key={index}
+              >
+                {word}
+              </li>;
+    });
+  }
+  let wordMapVerbs;
+  let verbType;
+  if (verbs.length) {
+    verbType = <li className='synonyms-type'>Verbs</li>;
+    const actualVerbs = verbs[0];
+    wordMapVerbs = actualVerbs.map(( word: string, index: number ) => {
+      return <li
+                className='synonyms-item'
+                onClick={ (e) => prop.onWordClick(e) }
+                key={index}
+              >
+                {word}
+              </li>;
+    });
+  }
+
   return (
     <ul className='synonyms' style={positionCSS} onMouseEnter={hover} onMouseLeave={removeHover}>
-      {wordMap}
+      {adjType}
+      {wordMapAdj}
+      {adverbType}
+      {wordMapAdverb}
+      {nounType}
+      {wordMapNouns}
+      {verbType}
+      {wordMapVerbs}
     </ul>
   );
 
